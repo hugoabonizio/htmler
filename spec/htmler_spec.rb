414f4bc97538a853c1ad509e415443a3d404f1d4
hugoabonizio <hugo_abonizio@hotmail.com>
2014-10-22 20:57:47 +0000

    more complicated test

--- spec/htmler_spec.rb
+++ spec/htmler_spec.rb
@@ -18,4 +18,9 @@ describe Htmler do
     @htmler.compile_file('spec/files/simple_html_head_title.htmler')
     expect(@htmler.to_html).to eql('<html><head><title>lol</title></head></html>')
   end
+  
+  it "should parse a more complex file" do
+    @htmler.compile_file('spec/files/moderate1.htmler')
+    expect(@htmler.to_html).to eql('<html><head><title>Hello world!</title></head><body><header><h1>Site title</h1></header><div><div><p>From the FIRST div</p></div><div><p>From the SECOND div</p><p>...and another one</p></div></div></body></html>')
+  end
 end
