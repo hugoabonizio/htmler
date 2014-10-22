448e0094e300f8b614cddd191546ad89173750ef
hugoabonizio <hugo_abonizio@hotmail.com>
2014-10-22 20:13:08 +0000

    add method_missing magic

---
+++ lib/htmler.rb
@@ -0,0 +1,30 @@
+require 'htmler/base'
+require 'html/version'
+
+class Htmler
+  undef_method :p
+  
+  def initialize
+    @_result = ''
+  end
+  
+  def to_html
+    @_result
+  end
+
+  def method_missing(name, *args, &block)
+    @_result += "<#{name}>"
+    if block_given?
+      _return = block.call
+      @_result +=  _return unless _return.nil?
+    end
+    @_result += "</#{name}>"
+    
+    # print the temporary stack
+    # puts "> #{@_result}"
+    
+    nil
+    
+  end
+  
+end
