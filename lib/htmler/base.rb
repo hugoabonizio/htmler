01721c84477c7252b48c8666be093e694e67483a
hugoabonizio <hugo_abonizio@hotmail.com>
2014-10-22 20:44:12 +0000

    move the class to base.rb

---
+++ lib/htmler/base.rb
@@ -0,0 +1,35 @@
+class Htmler
+  undef_method :p
+  
+  def initialize
+    @_result = ''
+  end
+  
+  def to_html
+    @_result
+  end
+
+  def method_missing(name, *args, &block)
+    @_result += "<#{name}>"
+    if block_given?
+      _return = block.call
+      @_result +=  _return unless _return.nil?
+    end
+    @_result += "</#{name}>"
+    
+    # print the temporary stack
+    # puts "> #{@_result}"
+    
+    nil
+    
+  end
+  
+  def compile(input)
+    instance_eval(input)
+  end
+  
+  def compile_file(name)
+    instance_eval(File.new(name).readlines.join)
+  end
+  
+end
